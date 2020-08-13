0b9ede59164726535895639110219392eddc31f6
chloechloe <chloeying84@gmail.com>
2020-08-13 22:39:58 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const port = 3000;
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/url-shortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 app.get("/", (req, res) => {
   res.send("hello world!");
---
+++ models/url.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Url", urlSchema);
